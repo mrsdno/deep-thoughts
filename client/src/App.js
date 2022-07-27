508c7a3786ebac75bcee9fbe9347974909710bee
mrsdno <christine@dno.dev>
2022-07-26 23:32:38 -0400

    added queries and populated data

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,14 @@ import Home from './pages/Home';
 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
@@ -19,14 +27,27 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
-      </div>
-      <Footer />
-      </div>
-      </ApolloProvider>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile">
+                <Route path=":username" element={<Profile />} />
+                <Route path="" element={<Profile />} />
+              </Route>
+              <Route path="/thought/:id" element={<SingleThought />} />
+
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ThoughtList = ( { thoughts, title } ) => {
     if (!thoughts.length) {
@@ -6,27 +7,34 @@ const ThoughtList = ( { thoughts, title } ) => {
     }
 
     return (
-        <div>
-            <h3>{title}</h3>
-            {thoughts &&
-                thoughts.map(thought => (
-
-                    // key prop to internally track which data needs to be re-rendered if something changes
-                    <div key={thought.id} className="carb mb-3">
-                        <p className="card-header">
-                            {thought.username}
-                            thought on {thought.createdAt}
-                        </p>
-                        <div className="card-body">
-                            <p>{thought.thoughtText}</p>
-                            <p className="mb-0">
-                                Reactions: {thought.reactionCount} || Click to{' '}
-                                {thought.reactionCount ? 'see' : 'start'} the discussion!
-                            </p>
-                        </div>
-                    </div>
-                ))}
-        </div>
+      <div>
+        <h3>{title}</h3>
+        {thoughts &&
+          thoughts.map((thought) => (
+            // key prop to internally track which data needs to be re-rendered if something changes
+            <div key={thought.id} className="carb mb-3">
+              <p className="card-header">
+                <Link
+                  to={`/profile/${thought.username}`}
+                  style={{ fontWeight: 700 }}
+                  className="text-light"
+                >
+                  {thought.username}
+                </Link>{" "}
+                thought on {thought.createdAt}
+              </p>
+              <div className="card-body">
+                <Link to={`/thought/${thought._id}`}>
+                  <p>{thought.thoughtText}</p>
+                  <p className="mb-0">
+                    Reactions: {thought.reactionCount} || Click to{" "}
+                    {thought.reactionCount ? "see" : "start"} the discussion!
+                  </p>
+                </Link>
+              </div>
+            </div>
+          ))}
+      </div>
     );
 };
 
